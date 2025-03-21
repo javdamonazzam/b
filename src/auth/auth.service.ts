625c20ac306383404199191f511310e2ea70baeb
javdamonazzam <m131037@gmail.com>
2025-03-22 03:06:23 +0330

    a

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,7 +9,7 @@ export class AuthService {
   constructor(
     private readonly usersService: UserService,
     private readonly jwtService: JwtService,
-  ) {}
+  ) { }
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOneWithPw(username);
 
@@ -19,45 +19,28 @@ export class AuthService {
     }
     return null;
   }
-  //   async login(Body: User) {
-  //     // const validateUser = await this.validateUser(Body.username,Body.password);
-  //     const user = await this.usersService.findOneWithPw(Body.username);
 
-  //     if (user && user.password && bcrypt.compareSync(Body.password, user.password)) {
-  //     const payload = {
-  //       username: Body.username,
-  //       role:Body.role
-  //     };
-  //     return {
-  //       access_token: this.jwtService.sign(payload, {
-  //         secret: allConfig.jwt.secret,
-  //       }),
-  //       user:Body.username,
-  //     };
-  //   }else{
-  //     throw new UnauthorizedException('نام کاربری یا رمز عبور اشتباه است!');
-
-  //   }
-  // }
 
   async login(users: User) {
     const { username, password } = users;
-    const user = await this.usersService.findOneBy({
-      username: users.username,
-    });
-    const validate = await this.validateUser(username, password);
-    if (validate == null) {
-      throw new UnauthorizedException('Invalid username or password');
-    }
-    const payload = { sub: users.id, username: username ,id:user.id ,role: user.role};
+    if (username) {
+      const user = await this.usersService.findOneBy({
+        username: users.username,
+      });
+      const validate = await this.validateUser(username, password);
+      if (validate == null) {
+        throw new UnauthorizedException('Invalid username or password');
+      }
+      const payload = { sub: users.id, username: username, id: user.id, role: user.role };
 
-    return {
-      username,
-      id: user.id,
-      role:user.role,
-      access_token: this.jwtService.sign(payload, {
-        secret: allConfig.jwt.secret,
-      }),
-    };
+      return {
+        username,
+        id: user.id,
+        role: user.role,
+        access_token: this.jwtService.sign(payload, {
+          secret: allConfig.jwt.secret,
+        }),
+      };
+    }
   }
 }
