10fea152afa1e9c98b7dbabb305eb1c4abb4a1ce
javdamonazzam <m131037@gmail.com>
2025-03-22 02:49:12 +0330

    A

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,69 +1,10 @@
-import { BaseEntity } from 'src/base/base.entity';
 import {
-  BeforeInsert,
-  Column,
-  Entity,
-  JoinTable,
-  ManyToMany,
-  OneToMany,
+    Column,
 } from 'typeorm';
-import * as bcrypt from 'bcrypt';
-import { RoleEnum } from 'src/types/enum/role.enum';
-import { Service } from '@/service/entities/service.entity';
-import { Invoice } from '@/invoice/entities/invoice.entity';
-import { Server } from '@/server/entities/server.entity';
-@Entity('user')
-export class User extends BaseEntity {
-  @Column({ type: 'varchar', length: 250 })
-  username: string;
-  @Column({ type: 'varchar', length: 250, nullable: true })
-  password: string;
+export class User {
+    @Column({ type: 'varchar', length: 250 })
+    username: string;
+    @Column({ type: 'varchar', length: 250, nullable: true })
+    password: string;
 
-  @Column('int')
-  account_price: number;
-
-  @Column('enum', { enum: RoleEnum, default: RoleEnum.USER })
-  role: RoleEnum[];
-
-  @BeforeInsert()
-  async hashPasswordAndSetTimeStamp() {
-    if (!this.password) return;
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  // relation
-  @OneToMany('Service', 'user')
-  service: Service[];
-
-  @OneToMany('Invoice', 'user')
-  invoice: Invoice[];
-
-  @ManyToMany(() => Server, (server) => server.users)
-  @JoinTable()
-  servers: Server[];
 }
-// import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-// import { Reflector } from '@nestjs/core';
-// import { ROLES_KEY } from '../decorators/role.decorator';
-// // import { UserInfoType } from 'src/auth/types/user-info.type';
-// import { isPublic } from 'src/auth/utils/auth-utils';
-// import { User } from 'src/user/entities/user.entity';
-
-// @Injectable()
-// export class RolesGuard implements CanActivate {
-//   constructor(private reflector: Reflector) {}
-
-//   canActivate(context: ExecutionContext): boolean {
-//     const request = context.switchToHttp().getRequest();
-//     const requiredRoles: string[] = this.reflector.getAllAndOverride(
-//       ROLES_KEY,
-//       [context.getHandler(), context.getClass()],
-//     );
-
-//     if (!requiredRoles || isPublic(context, this.reflector)) {
-//       return true;
-//     }
-//     const user: User = request.user;
-
-//     return requiredRoles.some((role) => +role === user.role);
-//   }
-// }
