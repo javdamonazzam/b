90cfdd6d5db56a4edb7f6ac4e9d859c59b496aaa
javdamonazzam <m131037@gmail.com>
2025-03-26 15:21:31 +0330

    f

--- src/tel_user/tel_user.controller.ts
+++ src/tel_user/tel_user.controller.ts
@@ -3,26 +3,20 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
-  Delete,
 } from '@nestjs/common';
 import { TelUserService } from './tel_user.service';
 import { CreateTelUserDto } from './dto/create-tel_user.dto';
-import { UpdateTelUserDto } from './dto/update-tel_user.dto';
 import { Public } from '@/auth/decorators/public.decorator';
 
 @Controller('tel')
 export class TelUserController {
   constructor(private readonly telUserService: TelUserService) {}
+
   @Public()
   @Post()
-  created(@Body() createTelUserDto: CreateTelUserDto) {
-    return this.telUserService.createuser(createTelUserDto);
+  async create(@Body() createTelUserDto: CreateTelUserDto) {
+    // return await this.telUserService.createUser(createTelUserDto);
   }
 
-  @Get()
-  findAll() {
-    return this.telUserService.findAll({});
-  }
 }
