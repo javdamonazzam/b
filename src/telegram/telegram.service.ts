02712a25cab3289aa1800017c5711955383c874c
javdamonazzam <m131037@gmail.com>
2025-04-04 23:59:01 +0330

    Update telegram.service.ts

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,30 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTelegramDto } from './dto/create-telegram.dto';
 import { UpdateTelegramDto } from './dto/update-telegram.dto';
+import { WalletService } from '@/wallet/wallet.service';
+import { UserService } from '@/user/user.service';
+import { ServiceService } from '@/service/service.service';
 
 @Injectable()
 export class TelegramService {
-  create(createTelegramDto: CreateTelegramDto) {
-
+  constructor(
+    private walletService: WalletService,
+    private userService: UserService,
+    private readonly serviceService: ServiceService,
+  ) { }
+  async start(body: any) {
+    console.log("start");
+    
+    const user = await this.userService.findByUsername(String(body.chatId))
+    console.log(user);
+    
+    if (!user) {
+      await this.userService.create_user({ username: `${body.chatId}`, password: "lkjalkjgkjla", account_price: 45000 })
+    }
+    const wallet = await this.walletService.findOneBy({user_id: user.id})
+    console.log(wallet.wallet_balance);
+    
+    return {
+      user_id:user.id,
+      wallet_balance: wallet.wallet_balance,
+      account_price: user.account_price,
+    };
   }
-  start(body:any) {
-console.log(body);
 
-  }
-
-  findAll() {
-    return `This action returns all telegram`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} telegram`;
-  }
-
-  update(id: number, updateTelegramDto: UpdateTelegramDto) {
-    return `This action updates a #${id} telegram`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} telegram`;
+ async create(body:any) {
+    console.log("start",body);
+   return await this.serviceService.create_account(body) 
   }
 }
