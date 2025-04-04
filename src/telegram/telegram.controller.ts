dbcc582e00a8a56d5772b62f27d38203fa332c7a
javdamonazzam <m131037@gmail.com>
2025-04-04 21:50:19 +0330

    a

--- src/telegram/telegram.controller.ts
+++ src/telegram/telegram.controller.ts
@@ -7,36 +7,17 @@ import { Public } from '@/auth/decorators/public.decorator';
 @Controller('telegram')
 export class TelegramController {
   constructor(private readonly telegramService: TelegramService) { }
+
   @Public()
   @Post('start')
   start(@Body() body: any) {
     return this.telegramService.start(body);
   }
 
-
+  @Public()
   @Post('config')
-  create(@Body() createTelegramDto: CreateTelegramDto) {
-    return this.telegramService.create(createTelegramDto);
-  }
-
-
-  @Get()
-  findAll() {
-    return this.telegramService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.telegramService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTelegramDto: UpdateTelegramDto) {
-    return this.telegramService.update(+id, updateTelegramDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.telegramService.remove(+id);
+  create(@Body() body: any) {
+    
+    return this.telegramService.create(body);
   }
 }
