7d8c1f5dd795b9f3ca6db5b61f54d82995e6ec61
javdamonazzam <m131037@gmail.com>
2025-04-29 16:37:43 +0330

    Update jwt.strategy.ts

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get('JWT_SECRET'),  // استفاده از ConfigService
+      secretOrKey: '@!!!jjjj',  // استفاده از ConfigService
     });
   }
 
