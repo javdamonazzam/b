584e258b0313cb18ac2cdc646808532cc4753b0c
javdamonazzam <m131037@gmail.com>
2025-04-29 16:42:52 +0330

    Update jwt.strategy.ts

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,19 +2,21 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';  // اضافه کردن ConfigService
-
+import { config } from 'dotenv';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
-  constructor(private configService: ConfigService) {
-    console.log('POSTGRES_USER:', process.env.POSTGRES_USER);
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: '@!!!jjjj',  // استفاده از ConfigService
-    });
-  }
+    constructor(private configService: ConfigService) {
+        const secret = configService.get<string>('JWT_SECRET');  // استفاده از ConfigService برای خواندن متغیر
 
-  async validate(payload: any) {
-    return { username: payload.username, role: payload.role };
-  }
+        console.log('JWT_SECRET:', secret);
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: '@!!!jjjj',  // استفاده از ConfigService
+        });
+    }
+
+    async validate(payload: any) {
+        return { username: payload.username, role: payload.role };
+    }
 }
